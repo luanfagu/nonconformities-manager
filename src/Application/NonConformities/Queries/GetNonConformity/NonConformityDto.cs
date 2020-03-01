f2f070f0f1034172991d8e8b32647a519385fb21
Luan Fagundes <luanfagu@gmail.com>
2020-02-29 22:53:14 -0300

    update nonConformityDto for mapping (not working yet)

--- src/Application/NonConformities/Queries/GetNonConformity/NonConformityDto.cs
+++ src/Application/NonConformities/Queries/GetNonConformity/NonConformityDto.cs
@@ -1,19 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
 using TesteQualyteam.Application.Common.Mappings;
 using TesteQualyteam.Domain.Entities;
 
 namespace TesteQualyteam.Application.NonConformities.Queries.GetNonConformity
 {
-    public class NonConformityDto: IMapFrom<NonConformity>
+    public class NonConformityDto : IMapFrom<NonConformity>
     {
         public NonConformityDto()
         {
             Actions = new List<ActionDto>();
         }
+
         public int Id { get; set; }
         public string Code { get; set; }
         public string Description { get; set; }
         public int Status { get; set; }
         public IList<ActionDto> Actions { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<NonConformity, NonConformityDto>()
+                .ForMember(d => d.Code,
+                    opt => opt.MapFrom(s =>
+                        (s.Year + ":" + CompleteLeft(s.Identity.ToString(), "0", 2) + ":" +
+                        CompleteLeft(s.Revision.ToString(), "0", 2))));
+        }
+
+        private string CompleteLeft(string input, string character, int desiredLength)
+        {
+            for (int i = input.Length; i < desiredLength; i++)
+            {
+                input = character + input;
+            }
+
+            return input;
+        }
     }
 }
